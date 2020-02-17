da5ca897151cc6ce6289d2cd5614329769b2ac89
muhalik <muhalik.net@gmail.com>
2020-02-17 21:13:57 +0500

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 
 const express = require('express')
 const next = require('next')
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3000
+// const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 5000
 const dev = process.env.NODE_DEV !== 'production' //true false
 const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler() //part of next config
+// const handle = nextApp.getRequestHandler() //part of next config
 const mongoose = require('mongoose')
-
-const db = mongoose.connect('mongodb://localhost:27017/Photos')
 
 nextApp.prepare().then(() => {
       const app = express();
